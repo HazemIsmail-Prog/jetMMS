798ea05bddfdb275927808c3a0e0d553a0e8b480
HazemIsmail-Prog <67516349+HazemIsmail-Prog@users.noreply.github.com>
2024-03-01 17:53:44 +0300

    fix services form

--- resources/views/livewire/services/service-form.blade.php
+++ resources/views/livewire/services/service-form.blade.php
@@ -5,59 +5,61 @@
         </x-slot>
 
         <x-slot name="content">
-            <form wire:submit="save">
+            @if ($showModal)
+                <form wire:submit="save">
 
-                <div class=" space-y-3">
-                    <div>
-                        <x-label for="name_ar">{{ __('messages.name_ar') }}</x-label>
-                        <x-input required class="w-full py-0" wire:model="form.name_ar" autocomplete="off" type="text"
-                            id="name_ar" />
-                        <x-input-error for="form.name_ar" />
+                    <div class=" space-y-3">
+                        <div>
+                            <x-label for="name_ar">{{ __('messages.name_ar') }}</x-label>
+                            <x-input required class="w-full py-0" wire:model="form.name_ar" autocomplete="off"
+                                type="text" id="name_ar" />
+                            <x-input-error for="form.name_ar" />
+                        </div>
+                        <div>
+                            <x-label for="name_en">{{ __('messages.name_en') }}</x-label>
+                            <x-input required class="w-full py-0" wire:model="form.name_en" autocomplete="off"
+                                type="text" id="name_en" />
+                            <x-input-error for="form.name_en" />
+                        </div>
+                        <div>
+                            <x-label for="min_price">{{ __('messages.min_price') }}</x-label>
+                            <x-input required class="w-full py-0" wire:model="form.min_price" autocomplete="off"
+                                type="number" id="min_price" />
+                            <x-input-error for="form.min_price" />
+                        </div>
+                        <div>
+                            <x-label for="max_price">{{ __('messages.max_price') }}</x-label>
+                            <x-input required class="w-full py-0" wire:model="form.max_price" autocomplete="off"
+                                type="number" id="max_price" />
+                            <x-input-error for="form.max_price" />
+                        </div>
+                        <div>
+                            <x-label for="department_id">{{ __('messages.department') }}</x-label>
+                            <x-searchable-select model="form.department_id" :list="$this->departments" position="relative" />
+                            <x-input-error for="form.department_id" />
+                        </div>
+                        <div>
+                            <x-label for="type">{{ __('messages.type') }}</x-label>
+                            <x-select required class="w-full py-0" wire:model="form.type" autocomplete="off"
+                                type="number" id="type">
+                                <option value="">---</option>
+                                <option value="service">{{ __('messages.service') }}</option>
+                                <option value="part">{{ __('messages.parts') }}</option>
+                            </x-select>
+                            <x-input-error for="form.type" />
+                        </div>
+                        <div>
+                            <x-label for="active" class="flex items-center">
+                                <x-checkbox wire:model="form.active" id="active" />
+                                <span class="ms-2 ">{{ __('messages.active') }}</span>
+                            </x-label>
+                        </div>
                     </div>
-                    <div>
-                        <x-label for="name_en">{{ __('messages.name_en') }}</x-label>
-                        <x-input required class="w-full py-0" wire:model="form.name_en" autocomplete="off"
-                            type="text" id="name_en" />
-                        <x-input-error for="form.name_en" />
+                    <div class="mt-3">
+                        <x-button>{{ __('messages.save') }}</x-button>
                     </div>
-                    <div>
-                        <x-label for="min_price">{{ __('messages.min_price') }}</x-label>
-                        <x-input required class="w-full py-0" wire:model="form.min_price" autocomplete="off"
-                            type="number" id="min_price" />
-                        <x-input-error for="form.min_price" />
-                    </div>
-                    <div>
-                        <x-label for="max_price">{{ __('messages.max_price') }}</x-label>
-                        <x-input required class="w-full py-0" wire:model="form.max_price" autocomplete="off"
-                            type="number" id="max_price" />
-                        <x-input-error for="form.max_price" />
-                    </div>
-                    <div>
-                        <x-label for="department_id">{{ __('messages.department') }}</x-label>
-                        <x-searchable-select model="form.department_id" :list="$this->departments" position="relative"/>
-                        <x-input-error for="form.department_id" />
-                    </div>
-                    <div>
-                        <x-label for="type">{{ __('messages.type') }}</x-label>
-                        <x-select required class="w-full py-0" wire:model="form.type" autocomplete="off" type="number"
-                            id="type">
-                            <option value="">---</option>
-                            <option value="service">{{ __('messages.service') }}</option>
-                            <option value="part">{{ __('messages.parts') }}</option>
-                        </x-select>
-                        <x-input-error for="form.type" />
-                    </div>
-                    <div>
-                        <x-label for="active" class="flex items-center">
-                            <x-checkbox wire:model="form.active" id="active" />
-                            <span class="ms-2 ">{{ __('messages.active') }}</span>
-                        </x-label>
-                    </div>
-                </div>
-                <div class="mt-3">
-                    <x-button>{{ __('messages.save') }}</x-button>
-                </div>
-            </form>
+                </form>
+            @endif
         </x-slot>
     </x-dialog-modal>
 </div>
