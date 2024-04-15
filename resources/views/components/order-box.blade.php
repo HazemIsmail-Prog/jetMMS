897c090d2190bedcf0019082eda2ae5f8d4a78c9
Hazem <hazem.ismail@hotmail.com>
2024-04-15 21:16:22 +0300

    some colors modifications

--- resources/views/components/order-box.blade.php
+++ resources/views/components/order-box.blade.php
@@ -3,45 +3,46 @@
 <div wire:key="order-{{ $order->id }}" id="order-{{ $order->id }}" data-index="{{ $order->index }}"
     class="
     {{ $order->in_progress ? '' : 'draggable' }}
-    {{ $order->unread_comments_count > 0 ? ' animate-wiggle hover:animate-none' : '' }}
+    {{-- {{ $order->unread_comments_count > 0 ? ' animate-wiggle hover:animate-none' : '' }} --}}
     order
-    border-4
-    py-2 lg:py-4
-    p-0 lg:p-4
+    border
+    py-2 lg:py-0
+    p-0 lg:p-0
     cursor-pointer
     rounded-lg
     "
-    style="border-color: {{ $order->status->color }}">
+    style="border-color: {{ $order->status->color }};background: {{ $order->status->color }};">
 
     <div wire:click="$dispatch('showCommentsModal',{order:{!! $order !!}})"
         class="block lg:hidden text-center text-xs font-medium select-none">
         {{ $order->formated_id }}
     </div>
+<div class="p-2 text-white">
+    
+            <div class="hidden lg:flex justify-between items-center">
+                <div class="text-md font-semibold truncate">{{ $order->customer->name }}</div>
+                <div class="text-xs">{{ $order->phone->number }}</div>
+            </div>
+            <h4 class="hidden lg:block mt-2 text-xs">{{ $order->address->full_address }}</h4>
+</div>
 
-    <div class="hidden lg:flex justify-between items-center">
-        <div class="text-md font-semibold">{{ $order->customer->name }}</div>
-        <div class="text-xs">{{ $order->phone->number }}</div>
-    </div>
-
-    <h4 class="hidden lg:block mt-2 text-xs">{{ $order->address->full_address }}</h4>
-
-    <div class="hidden lg:block mt-0 lg:mt-2">
+    <div class="hidden lg:block mt-0 lg:mt-2 bg-white p-2 rounded-t-lg dark:bg-gray-800  text-gray-950 dark:text-gray-200">
 
         <div
-            class="items-center my-1 bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400">
+            class="items-center my-1 bg-gray-100 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700">
             {{ $order->creator->name }}
         </div>
 
         <div
-            class="items-center my-1 bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400">
+            class="items-center my-1 bg-gray-100 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700">
             {{ $order->formated_id }}
         </div>
 
         @if (!$order->in_progress)
             <div x-data={}
-                class="flex items-center my-1 bg-gray-100 text-gray-800 text-xs font-medium p-0 rounded dark:bg-gray-700 dark:text-gray-400">
+                class="flex items-center my-1 bg-gray-100 text-xs font-medium p-0 rounded dark:bg-gray-700">
                 <x-select @change="$wire.changeTechnician($event.target.value,{{ $order['id'] }})"
-                    class="!h-auto text-start border-none w-full focus:ring-0 bg-gray-100 text-gray-800 text-xs font-medium p-0 rounded dark:bg-gray-700 dark:text-gray-400 ">
+                    class="!h-auto text-start border-none w-full focus:ring-0 bg-gray-100 text-xs font-medium p-0 rounded dark:bg-gray-700 ">
                     @if ($order->status_id != 2)
                         <option value="">---</option>
                     @endif
@@ -56,21 +57,21 @@
 
         @if ($order->order_description)
             <div
-                class="flex items-center my-1 bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400">
+                class="flex items-center my-1 bg-gray-100 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700">
                 {{ $order->order_description }}
             </div>
         @endif
 
         @if ($order->notes)
             <div
-                class="flex items-center my-1 bg-gray-100 text-gray-800 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700 dark:text-gray-400">
+                class="flex items-center my-1 bg-gray-100 text-xs font-medium px-2.5 py-0.5 rounded dark:bg-gray-700">
                 {{ $order->notes }}
             </div>
         @endif
 
     </div>
 
-    <div class="hidden lg:flex items-center justify-between w-full mt-3 text-xs font-medium text-gray-400">
+    <div class="hidden lg:flex items-center justify-between w-full mt-px text-xs font-medium bg-white dark:bg-gray-800 p-2 rounded-b-lg text-gray-950 dark:text-gray-200">
 
         <div class=" flex gap-2 items-center">
 
--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,6 +1,6 @@
 <div class=" overflow-x-auto overflow-clip border border-gray-300 dark:border-gray-700 rounded-lg">
     <table {!! $attributes->merge([
-        'class' => 'w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400',
+        'class' => 'w-full text-sm text-left rtl:text-right text-gray-800 dark:text-gray-200',
     ]) !!}>
         {{ $slot }}
     </table>
