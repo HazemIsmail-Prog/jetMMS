9bb5baff041d6c890ac0b9f6d9e5d0751d4c14a7
Hazem <hazem.ismail@hotmail.com>
2024-04-29 20:34:28 +0300

    wire navigate to nested sidebar items

--- resources/views/components/nested-sidebar-item.blade.php
+++ resources/views/components/nested-sidebar-item.blade.php
@@ -10,7 +10,7 @@
 
 
 <li class="mb-1 last:mb-0">
-    <a class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if ($active) {{ '!text-indigo-500 active' }} @endif"
+    <a wire:navigate class="block text-slate-400 hover:text-slate-200 transition duration-150 truncate @if ($active) {{ '!text-indigo-500 active' }} @endif"
         href="{{ route($route, $param) }}">
         <span class="text-sm font-medium duration-200">{{ $title }}</span>
     </a>
