645ac16c9da647c8eb68d88353446f8881de192c
Hazem <hazem.ismail@hotmail.com>
2024-04-29 20:27:16 +0300

    test with wire navigate

--- resources/views/components/sidebar-item.blade.php
+++ resources/views/components/sidebar-item.blade.php
@@ -3,7 +3,7 @@
     $active = explode('.', request()->route()->getName())[0] == explode('.', $route)[0];
 @endphp
 <li class="px-3 py-2 rounded-lg mb-0.5 last:mb-0 @if ($active) {{ 'bg-slate-900' }} @endif">
-    <a class="block truncate transition duration-150" href="{{ route($route) }}">
+    <a wire:navigate class="block truncate transition duration-150" href="{{ route($route) }}">
         <div class="flex items-center {{ $active ? 'text-indigo-500' : 'text-slate-400 hover:text-white' }}">
             <x-dynamic-component :component="'svgs.' . $icon" class="w-6 h-6" />
             <span class="text-sm font-medium ms-3 duration-200">{{ $title }}</span>
