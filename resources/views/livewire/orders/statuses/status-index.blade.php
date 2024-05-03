306c1e812e66a231fef25a8a344285042b28df25
Hazem <hazem.ismail@hotmail.com>
2024-05-03 09:18:07 +0300

    wrap status table

--- resources/views/livewire/orders/statuses/status-index.blade.php
+++ resources/views/livewire/orders/statuses/status-index.blade.php
@@ -17,13 +17,13 @@
                 @foreach ($this->statuses as $status)
                     <x-tr>
                         <x-td style="color: {{ $status->status->color }}">{{ $status->status->name }}</x-td>
-                        <x-td>{{ @$status->reason ?? '-' }}</x-td>
-                        <x-td>{{ @$status->technician->name ?? '-' }}</x-td>
+                        <x-td class=" !whitespace-normal">{{ @$status->reason ?? '-' }}</x-td>
+                        <x-td class=" !whitespace-normal">{{ @$status->technician->name ?? '-' }}</x-td>
                         <x-td>
                             <div>{{ $status->created_at->format('d-m-Y') }}</div>
                             <div class=" text-xs">{{ $status->created_at->format('H:i') }}</div>
                         </x-td>
-                        <x-td>{{ @$status->creator->name }}</x-td>
+                        <x-td class=" !whitespace-normal">{{ @$status->creator->name }}</x-td>
                     </x-tr>
                 @endforeach
             </tbody>
