5f4ab6c3f4abd944eb7800b7f3910624ba4f19d6
Hazem <hazem.ismail@hotmail.com>
2024-05-04 15:48:31 +0300

    reduce drag delay

--- resources/views/livewire/dispatching/dispatching-index.blade.php
+++ resources/views/livewire/dispatching/dispatching-index.blade.php
@@ -94,7 +94,7 @@
             const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body
                 .clientWidth;
             const delay = screenWidth < 1024 ? 500 :
-                250; // If less than 1024 pixels, set delay to 500, otherwise set to 250
+                0; // If less than 1024 pixels, set delay to 500, otherwise set to 0
 
             sortable = new Sortable.create(element, {
                 group: 'box', // set both lists to same group
