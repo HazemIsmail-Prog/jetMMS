a8726aaddd19893239506d1ac38d188d8ab249a9
Hazem <hazem.ismail@hotmail.com>
2024-05-06 18:47:58 +0300

    hide vouchers

--- app/Livewire/Vouchers/VoucherIndex.php
+++ app/Livewire/Vouchers/VoucherIndex.php
@@ -46,7 +46,7 @@ class VoucherIndex extends Component
     {
         return Voucher::query()
             ->latest()
-            // ->where('type','jv')
+            ->where('type','jv')
             ->with('user')
             ->when($this->filters['search'],function(Builder $q){
                 $q->where(function(Builder $q){
