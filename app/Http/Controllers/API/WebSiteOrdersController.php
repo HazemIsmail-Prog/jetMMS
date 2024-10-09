82a7604b7bce396a92c553533424942ea9a44cba
Hazem <hazem.ismail@hotmail.com>
2024-10-09 15:07:27 +0300

    fix

--- app/Http/Controllers/API/WebSiteOrdersController.php
+++ app/Http/Controllers/API/WebSiteOrdersController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Area;
 use Illuminate\Http\Request;
 
 class WebSiteOrdersController extends Controller
 {
-    public function store() {
-        return 'OK From Hazem';
+    public function store()
+    {
+        
+        return 'OKKKKKKK';
     }
 }
