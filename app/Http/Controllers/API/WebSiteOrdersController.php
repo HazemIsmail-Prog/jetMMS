d2d5eabe65aa62d024746dd4944cfbb6b59cbdc7
Hazem <hazem.ismail@hotmail.com>
2024-10-09 15:17:58 +0300

    fix

--- app/Http/Controllers/API/WebSiteOrdersController.php
+++ app/Http/Controllers/API/WebSiteOrdersController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use App\Models\Area;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class WebSiteOrdersController extends Controller
 {
-    public function store()
+    public function store(Request $request)
     {
-    
 
+        Log::info($request->all());
         return response('OK');
-        }
+    }
 }
