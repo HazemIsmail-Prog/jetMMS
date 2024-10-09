ebc7923dbe2dfda09f11ccb740302025a0c140a0
Hazem <hazem.ismail@hotmail.com>
2024-10-09 14:18:21 +0300

    create website order contoller an api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AreaController;
+use App\Http\Controllers\Api\WebSiteOrdersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,3 +21,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get('/areas',[AreaController::class,'index']);
+Route::post('onlineOrder', [WebSiteOrdersController::class,'store'])
