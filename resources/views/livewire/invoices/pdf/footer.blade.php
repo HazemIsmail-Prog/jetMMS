6e7ecfd9c5fa5aa436d9ce92ed0f1e718e59b8cf
Hazem <hazem.ismail@hotmail.com~>
2025-05-07 20:31:37 +0300

    update invoice footer

--- resources/views/livewire/invoices/pdf/footer.blade.php
+++ resources/views/livewire/invoices/pdf/footer.blade.php
@@ -1,4 +1,6 @@
 <div style="font-size: 0.8rem;text-align:center">
+    <p>كفالة على العمل ١٠ أيام فقط </P>
+    <p>الكفالة لا تشمل أعمال تسليك الصرف الصحي او أعمال الالتماس الكهربائي</p>
     <div>{{ config('app.name') }}</div>
     <div>{{ $settings->address_ar }}</div>
     <div>{{ $settings->address_en }}</div>
